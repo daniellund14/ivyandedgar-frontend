708fb245460544586258bfa1b7169407ad589e95
daniellund <daniel.lund14@gmail.com>
2019-09-02 17:36:24 -0600

    [IVY-15] Moved all components to the atomic design

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,27 @@
-import Alert from './Alert';
-import Checkbox from './Inputs/Checkbox.vue';
-import Radio from './Inputs/Radio.vue';
-import FormGroupInput from './Inputs/formGroupInput.vue';
-import Switch from './Switch.vue';
-import Badge from './Badge';
-import Progress from './Progress.vue';
-import Button from './Button.vue';
+import Alert from './atoms/Alert';
+import Checkbox from './atoms/Inputs/Checkbox.vue';
+import Radio from './atoms/Inputs/Radio.vue';
+import FormGroupInput from './atoms/Inputs/formGroupInput.vue';
+import Switch from './atoms/NavBar/Switch.vue';
+import Badge from './atoms/Badge';
+import Progress from './atoms/NavBar/Progress.vue';
+import Button from './atoms/Button.vue';
 
-import DropDown from './Dropdown.vue';
+import DropDown from './atoms/Dropdown.vue';
 
-import Card from './Cards/Card.vue';
-import Collapse from './Collapse/Collapse.vue';
-import CollapseItem from './Collapse/CollapseItem.vue';
+import Card from './atoms/Cards/Card.vue';
+import Collapse from './atoms/Collapse/Collapse.vue';
+import CollapseItem from './atoms/Collapse/CollapseItem.vue';
 import Navbar from './organisms/Navbar/Navbar';
 import NavLink from './atoms/NavBar/NavLink';
 import NavbarToggleButton from './atoms/NavBar/NavbarToggleButton';
-import TabPane from './Tabs/Tab.vue';
-import Tabs from './Tabs/Tabs.vue';
-import Modal from './Modal.vue';
-import Parallax from './Parallax.vue';
-import Slider from './Slider.vue';
+import TabPane from './atoms/Tabs/Tab.vue';
+import Tabs from './atoms/Tabs/Tabs.vue';
+import Modal from './atoms/NavBar/Modal.vue';
+import Parallax from './atoms/NavBar/Parallax.vue';
+import Slider from './atoms/NavBar/Slider.vue';
 
-import Pagination from './Pagination.vue';
+import Pagination from './atoms/NavBar/Pagination.vue';
 
 export {
   Alert,
